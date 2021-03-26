f9bda97ff40a96806bac25f6fa5826047c1c414e
andkafrynto <80204392+andkafrynto@users.noreply.github.com>
2021-03-26 23:07:37 +0700

    Add files via upload

---
+++ backgroundkuis.js
@@ -0,0 +1,108 @@
+<style>
+
+ body {
+    font-family: Open Sans;
+}
+
+
+
+h1 {
+
+    text-align: center;
+}
+
+
+
+#title {
+
+    text-decoration: underline;
+}
+
+
+
+#quiz {
+
+    text-indent: 10px;
+    display:none;
+}
+
+
+
+.button {
+
+    border:4px solid;
+    border-radius:5px;
+    width: 40px;
+    padding-left:5px;
+    padding-right: 5px;
+    position: relative;
+    float:right;
+    background-color: #DCDCDC;
+    color: black;
+    margin: 0 2px 0 2px;
+}
+
+
+
+.button.active {
+
+    background-color: ##FFA07A;
+    color: ##FFA07A;
+}
+
+
+
+button {
+
+    position: relative;
+    float:right;
+}
+
+
+
+.button a {
+
+    text-decoration: none;
+    color: blue;
+}
+
+
+
+#container {
+
+    width:50%;
+    margin:auto;
+    padding: 0 25px 40px 10px;
+    background-color: #827b7f;
+    border:4px solid ##FFA07A;
+    border-radius:5px;
+    color: #FFFFFF;
+    font-weight: bold;
+    box-shadow: 5px 5px 5px #888;
+}
+
+
+
+ul {
+
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+
+
+#prev {
+
+    display:#FFA07A;
+}
+
+
+
+#start {
+
+    display:none;
+    width: 90px;
+}
+#berjalan{font-family: Roboto;font-weight: bold;font-size: 20px;transition-duration: 0.0.5s;}
+</style>
